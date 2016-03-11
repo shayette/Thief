f410c759846ecddf0aa019d64c4329d44a949849
Sierra Pack <sierrajpack@gmail.com>
2016-03-10 19:19:16 -0700

    3/9/16 Team Assignment

--- thiefGame/src/byui/cit260/thiefGame/control/GameControl.java
+++ thiefGame/src/byui/cit260/thiefGame/control/GameControl.java
@@ -5,7 +5,11 @@
  */
 package byui.cit260.thiefGame.control;
 
+import byui.cit260.thiefGame.model.Game;
+import byui.cit260.thiefGame.model.ItemsToSteal;
+import byui.cit260.thiefGame.model.Map;
 import byui.cit260.thiefGame.model.Player;
+import thiefgame.ThiefGame;
 
 /**
  *
@@ -14,7 +18,90 @@ import byui.cit260.thiefGame.model.Player;
 public class GameControl {
 
     public static void createNewGame(Player player) {
-        System.out.println("\n*** createNewGame stub function called ***");
+        
+        Game game = new Game(); //create new game
+        ThiefGame.setCurrentGame(game); //save in ThiefGame
+        
+        game.setPlayer(player); //save player in game
+       
+        //create Items To Steal and save in game
+        ItemsToSteal[] itemsToSteal = GameControl.createItemsToStealList();
+        game.setItemsToStealList(itemsToSteal);
+        
+        Map map = MapControl.createMap(); //create and initialize new map
+        game.setMap(map); //save map in game
+        
+        //move actors to starting positiion in map
+        MapControl.moveActorsToStartingLocation(map);
     }
 
+    public static ItemsToSteal[] createItemsToStealList() {
+    //created array(list) of items
+    ItemsToSteal[] item = 
+            new ItemsToSteal[Constants.NUMBER_OF_ITEMS_TO_STEAL];
+    
+    ItemsToSteal painting = new ItemsToSteal();
+    painting.setDescription("Painting");
+    painting.setQuantityInStock(0);
+    painting.setRequiredAmount(0);
+    item[Item.painting.ordinal()] = painting;
+    
+    ItemsToSteal diamond = new ItemsToSteal();
+    diamond.setDescription("Diamond");
+    diamond.setQuantityInStock(0);
+    diamond.setRequiredAmount(0);
+    item[Item.diamond.ordinal()] = diamond;
+    
+    ItemsToSteal sculpture = new ItemsToSteal();
+    sculpture.setDescription("Sculpture");
+    sculpture.setQuantityInStock(0);
+    sculpture.setRequiredAmount(0);
+    item[Item.sculpture.ordinal()] = sculpture;
+    
+    ItemsToSteal gold = new ItemsToSteal();
+    gold.setDescription("Gold");
+    gold.setQuantityInStock(0);
+    gold.setRequiredAmount(0);
+    item[Item.gold.ordinal()] = gold;
+    
+    ItemsToSteal vase = new ItemsToSteal();
+    vase.setDescription("Vase");
+    vase.setQuantityInStock(0);
+    vase.setRequiredAmount(0);
+    item[Item.vase.ordinal()] = vase;
+    
+    return item;
+    }
+
+    public static ItemsToSteal[] getItemsToStealList() {
+        System.out.println("\n*** getItemsToStealList stub function called ***");
+        return null;
+    }
+
+    static void assignScenesToLocations(Map map, MapControl.Scenes[] scenes) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static void assignScenesToLocations(Map map, MapControl.Scene[] scenes) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private static class Constants {
+
+        private static int NUMBER_OF_ITEMS_TO_STEAL;
+
+        public Constants() {
+        }
+    }
+    
+    public enum Item {
+        painting, 
+        diamond,
+        sculpture, 
+        gold, 
+        vase;
+    }
+
+
+    
 }
