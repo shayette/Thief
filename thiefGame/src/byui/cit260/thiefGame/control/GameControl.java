c3d1d0da4ea2a9bb253783d4c5ac650bd143b09d
Shayna <Shayna@192.168.0.4>
2016-03-11 16:53:09 -0700

    3/11/2016 GameControl update

--- thiefGame/src/byui/cit260/thiefGame/control/GameControl.java
+++ thiefGame/src/byui/cit260/thiefGame/control/GameControl.java
@@ -7,11 +7,8 @@ package byui.cit260.thiefGame.control;
 
 import byui.cit260.thiefGame.model.Game;
 import byui.cit260.thiefGame.model.ItemsToSteal;
-import byui.cit260.thiefGame.model.Location;
 import byui.cit260.thiefGame.model.Map;
 import byui.cit260.thiefGame.model.Player;
-import byui.cit260.thiefGame.model.SceneType;
-import byui.cit260.thiefGame.model.Item;
 import thiefgame.ThiefGame;
 
 /**
@@ -36,6 +33,14 @@ public class GameControl {
         
         //move actors to starting positiion in map
         MapControl.moveActorsToStartingLocation(map);
+    }
+    
+    public enum Item {
+        painting,
+        diamond,
+        sculpture,
+        gold, 
+        vase;
     }
 
     public static ItemsToSteal[] createItemsToStealList() {
@@ -76,45 +81,11 @@ public class GameControl {
     return itemType;
     }
 
-
-        static void assignScenesToLocations(Map map, MapControl.Scene[] scenes) {
-        Location[][] locations = map.getLocations();
-        
-        //start point
-        locations[0][0].setScene(scenes[SceneType.start.ordinal()]);
-        locations[0][1].setScene(scenes[SceneType.loadingDock.ordinal()]);
-        locations[0][2].setScene(scenes[SceneType.exhibitRoom1.ordinal()]);
-        locations[0][3].setScene(scenes[SceneType.sculptureRoom.ordinal()]);
-        locations[0][4].setScene(scenes[SceneType.securityRoom.ordinal()]);
-        locations[1][0].setScene(scenes[SceneType.bathrooms.ordinal()]);
-        locations[1][2].setScene(scenes[SceneType.hallway1.ordinal()]);
-        locations[1][3].setScene(scenes[SceneType.hallway2.ordinal()]);
-        locations[1][4].setScene(scenes[SceneType.mainEntry.ordinal()]);
-        locations[1][5].setScene(scenes[SceneType.basementStairs.ordinal()]);
-        locations[2][0].setScene(scenes[SceneType.exhibitRoom2.ordinal()]);
-        locations[2][1].setScene(scenes[SceneType.exhibitRoom3.ordinal()]);
-        locations[2][2].setScene(scenes[SceneType.lobby.ordinal()]);
-        locations[2][3].setScene(scenes[SceneType.controlRoom.ordinal()]);
-        locations[2][4].setScene(scenes[SceneType.diamondRoom.ordinal()]);
-        locations[3][0].setScene(scenes[SceneType.admin.ordinal()]);
-        locations[3][1].setScene(scenes[SceneType.safeRoom.ordinal()]);
-        locations[3][2].setScene(scenes[SceneType.janitorCloset.ordinal()]);
-        locations[3][3].setScene(scenes[SceneType.laserHallway.ordinal()]);
-        locations[3][4].setScene(scenes[SceneType.exhibitRoom4.ordinal()]);
-        locations[4][0].setScene(scenes[SceneType.giftShop.ordinal()]);
-        locations[4][1].setScene(scenes[SceneType.basement.ordinal()]);
-        locations[4][2].setScene(scenes[SceneType.hallway3.ordinal()]);
-        locations[4][3].setScene(scenes[SceneType.vaseRoom.ordinal()]);
-        locations[4][4].setScene(scenes[SceneType.finish.ordinal()]);
-    }
-
     public static ItemsToSteal[] getItemsToStealList() {
          System.out.println("\n*** called getItemsToStealList in GameControl ***");
          return null;
     }
         
-        
-
     private static class Constants {
 
         private static int NUMBER_OF_ITEMS_TO_STEAL;
