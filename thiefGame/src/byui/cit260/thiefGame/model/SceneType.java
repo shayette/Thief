b79e03fd0a9564a93d381a34b3a486d14ab17bcf
Sierra Pack <sierrajpack@gmail.com>
2016-03-15 23:58:06 -0600

    3/15/16 SceneType Class

---
+++ thiefGame/src/byui/cit260/thiefGame/model/SceneType.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.thiefGame.model;
+
+/**
+ *
+ * @author sierrajane
+ */
+    public enum SceneType {
+    start, 
+    loadingDock, 
+    exhibitRoom1,
+    sculptureRoom,
+    securityRoom,
+    bathrooms,
+    hallway1,
+    hallway2,
+    mainEntry,
+    basementStairs,
+    exhibitRoom2,
+    exhibitRoom3,
+    lobby,
+    controlRoom,
+    diamondRoom,
+    admin,
+    safeRoom,
+    janitorCloset,
+    laserHallway,
+    exhibitRoom4,
+    giftShop,
+    basement,
+    hallway3,
+    hallway4,
+    vaseRoom,
+    finish;
+}
